12858ecf18222d34ed71817e50771ff3fc9a9d5d
mridul-outscal <mridul.agrawal@outscal.com>
2023-06-20 16:20:32 +0530

    Updated GameService

---
+++ Assets/Scripts/Main/GameService.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using ServiceLocator.Utilities;
+using ServiceLocator.Map;
+using ServiceLocator.Wave;
+using ServiceLocator.Sound;
+using ServiceLocator.Bloon;
+using ServiceLocator.Player;
+using ServiceLocator.Player.Projectile;
+
+namespace ServiceLocator.Main
+{
+    public class GameService : GenericMonoSingleton<GameService>
+    {
+        // Services:
+        private MapService mapService;
+        public MapService MapService => mapService;
+
+        private WaveService waveService;
+        public WaveService WaveService => waveService;
+        
+        private SoundService soundService;
+        public SoundService SoundService => soundService;
+
+        private PlayerService playerService;
+        public PlayerService PlayerService => playerService;
+
+
+        // Scriptable Objects:
+        [SerializeField] private MapScriptableObject mapScriptableObject;
+        [SerializeField] private WaveScriptableObject waveScriptableObject;
+        [SerializeField] private BloonTypePrefabMap bloonTypePrefabMap;
+        [SerializeField] private SoundScriptableObject soundScriptableObject;
+        [SerializeField] private MonkeyTypePrefabMap monkeyTypePrefabMap;
+        [SerializeField] private ProjectileTypePrefabMap projectileTypePrefabMap;
+
+        // Scene Referneces:
+        [SerializeField] private AudioSource SFXSource;
+        [SerializeField] private AudioSource BGSource;
+
+        private void Start()
+        {
+            mapService = new MapService(mapScriptableObject);
+            waveService = new WaveService(waveScriptableObject, bloonTypePrefabMap.BloonConfigurations);
+            soundService = new SoundService(soundScriptableObject, SFXSource, BGSource);
+            playerService = new PlayerService(monkeyTypePrefabMap.monkeyConfigurations, projectileTypePrefabMap.projectileConfigurations);
+
+        }
+    } 
+}
