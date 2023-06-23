353c73d4a271d313c13831983036487cc6d4bb20
mridul-outscal <mridul.agrawal@outscal.com>
2023-06-23 12:19:10 +0530

    Implemented logic for dragging & spawning monkeys

---
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace ServiceLocator.UI
+{
+    public class MonkeyImageHandler : MonoBehaviour, IPointerDownHandler, IDragHandler, IEndDragHandler
+    {
+        private RectTransform rectTransform;
+        private Image monkeyImage;
+        private MonkeyCellController owner;
+
+        private Sprite spriteToSet;
+        private Vector2 originalAnchoredPosition;
+        private Vector3 originalPosition;
+
+        public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
+        {
+            this.spriteToSet = spriteToSet;
+            this.owner = owner;
+        }
+
+        private void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            monkeyImage = GetComponent<Image>();
+            monkeyImage.sprite = spriteToSet;
+            originalPosition = rectTransform.localPosition;
+            originalAnchoredPosition = rectTransform.anchoredPosition;
+        }
+
+        public void OnPointerDown(PointerEventData eventData) => monkeyImage.color = new Color(1, 1, 1, 0.6f);
+
+        public void OnDrag(PointerEventData eventData) => rectTransform.anchoredPosition += eventData.delta;
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMonkeyImage();
+            owner.MonkeyDroppedAt(eventData.position);
+        }
+
+        private void ResetMonkeyImage()
+        {
+            monkeyImage.color = new Color(1, 1, 1, 1f);
+            rectTransform.anchoredPosition = originalAnchoredPosition;
+            rectTransform.localPosition = originalPosition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeySelectionUIController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ServiceLocator.UI
+{
+    public class MonkeySelectionUIController
+    {
+        private Transform cellContainer;
+        private List<MonkeyCellController> monkeyCellControllers;
+
+        public MonkeySelectionUIController(Transform cellContainer, MonkeyCellView monkeyCellPrefab, List<MonkeyCellScriptableObject> monkeyCellScriptableObjects)
+        {
+            this.cellContainer = cellContainer;
+            monkeyCellControllers = new List<MonkeyCellController>();
+
+            foreach (MonkeyCellScriptableObject monkeySO in monkeyCellScriptableObjects)
+            {
+                MonkeyCellController monkeyCell = new MonkeyCellController(cellContainer, monkeyCellPrefab, monkeySO);
+                monkeyCellControllers.Add(monkeyCell);
+            }
+        }
+
+        public void SetActive(bool setActive) => cellContainer.gameObject.SetActive(setActive);
+
+    }
+}
