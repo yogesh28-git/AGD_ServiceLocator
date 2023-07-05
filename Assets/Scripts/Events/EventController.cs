8b0714db5ef9b6788633147e65746f3df2c84a4a
mridul-outscal <mridul.agrawal@outscal.com>
2023-07-05 12:09:49 +0530

    Link changed for courses.

--- Assets/Scripts/Events/EventController.cs
+++ Assets/Scripts/Events/EventController.cs
@@ -1,10 +1,10 @@
 using System;
 
-/*  This script demonstrates implementation of the Observer Pattern.
+/**  This script demonstrates implementation of the Observer Pattern.
  *  If you're interested in learning about Observer Pattern, 
  *  you can find a dedicated course on Outscal's website.
- *  Link: https://outscal.com/lms/course/horror-escape-game-using-observer-pattern-using-unity/module/6465e1084c39da1772f30cf9?chapterId=6465e17f4c39da1772f30d09&materialId=6465e3374c39da1772f31bc3
- * */
+ *  Link: https://outscal.com/courses
+ **/
 
 namespace ServiceLocator.Events
 {
