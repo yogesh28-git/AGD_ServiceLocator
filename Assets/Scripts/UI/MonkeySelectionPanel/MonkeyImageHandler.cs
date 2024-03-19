196b964f77551730896afb3c0f0e4497e1543a55
Yogesh <yogeshsuresh28@gmail.com>
2024-03-19 20:51:11 +0530

    Monkey Drag and Drop functionality

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IDropHandler, IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
+        private RectTransform rectTransform;
+        private Vector2 originalAnchoredPosition;
+        private Vector2 originalPosition;
+        private LayoutElement layoutElement;
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -22,6 +26,40 @@ namespace ServiceLocator.UI
         {
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
+
+            rectTransform = GetComponent<RectTransform>( );
+            originalAnchoredPosition = rectTransform.anchoredPosition;
+            originalPosition = rectTransform.position;
+
+            layoutElement = GetComponent<LayoutElement>();
+        }
+
+        public void OnDrag( PointerEventData eventData )
+        {
+            rectTransform.position = eventData.position;
+            owner.MonkeyDraggedAt( eventData.position );
+        }
+
+        public void OnDrop( PointerEventData eventData )
+        {
+            ResetMonkey( );
+            owner.MonkeyDroppedAt( eventData.position );
+        }
+
+        public void ResetMonkey( )
+        {
+            monkeyImage.color = new Color( 1, 1, 1, 1 );
+
+            rectTransform.anchoredPosition = originalAnchoredPosition;
+            rectTransform.position = originalPosition;
+
+            layoutElement.enabled = false;
+            layoutElement.enabled = true;
+        }
+
+        public void OnPointerDown( PointerEventData eventData )
+        {
+            monkeyImage.color = new Color( 1, 1, 1, 0.6f );
         }
     }
 }
